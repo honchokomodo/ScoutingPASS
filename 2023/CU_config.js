bbe81b7a763d86ac7a632bafe0ffd15430f94065
Alex Levin <alevin@gmail.com>
2023-03-17 15:24:42 -0700

    Revert "make event field writable"
    This reverts commit a2158491eea8f5dd554c652419b9eaab6110e571.

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -16,6 +16,7 @@ var config_data = `
       "type": "event",
       "defaultValue": "2022carv",
       "required": "true",
+      "disabled": "true"
     },
     { "name": "Match Level",
       "code": "l",
